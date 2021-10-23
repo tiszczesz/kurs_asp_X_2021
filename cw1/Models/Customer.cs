99cb6131574011a6b2b04a1d5c4fae1b4997bfca
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-10-23 13:23:46 +0200

    customers

---
+++ cw1/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace cw1.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
--- cw1/Pages/Index.cshtml.cs
+++ cw1/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,9 @@ namespace cw1
 {
     public class IndexModel : PageModel
     {
+        public List<Customer> Customers { get; set; }
+       
+
         public void OnGet()
         {
         }
