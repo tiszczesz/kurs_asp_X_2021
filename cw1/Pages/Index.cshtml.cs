0ead9c37e4092de02e0444e7cff0a822fa8a0185
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-10-23 13:46:14 +0200

    table

--- cw1/Pages/Index.cshtml.cs
+++ cw1/Pages/Index.cshtml.cs
@@ -10,8 +10,18 @@ namespace cw1
 {
     public class IndexModel : PageModel
     {
+        // [BindProperty]
         public List<Customer> Customers { get; set; }
-       
+       public IndexModel()
+       {
+           Customers = new List<Customer>{
+               new Customer{FirstName = "Antoni",LastName = "Wołecki",Age=23},
+               new Customer{FirstName = "Monika",LastName = "Gruszka",Age=22},
+               new Customer{FirstName = "Tomasz",LastName = "Bomasz",Age=33},
+               new Customer{FirstName = "Roman",LastName = "Gałecki",Age=31},
+               new Customer{FirstName = "Grażyna",LastName = "Sprężyna",Age=54}
+           };
+       }
 
         public void OnGet()
         {
