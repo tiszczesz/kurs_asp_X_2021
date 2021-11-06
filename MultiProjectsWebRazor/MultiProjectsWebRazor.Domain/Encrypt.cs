2649fc37440b6170dad29fc229b68cf4b58e180c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-11-06 13:03:57 +0100

    ff

--- MultiProjectsWebRazor/MultiProjectsWebRazor.Domain/Encrypt.cs
+++ MultiProjectsWebRazor/MultiProjectsWebRazor.Domain/Encrypt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace MultiProjectsWebRazor.Domain
 {
     public class Encrypt
     {
+        
+        
+        [Display(Name = "Tekst do zakodowania")]
+        [Required(ErrorMessage = "Podaj Tekst")]
+        [DataType(DataType.MultilineText)]
         public string PlainText { get; set; }
         public string EncodingText { get; set; }
         public string DecodingText { get; set; }
-        public int Key { get; set; }
+
+        [Display(Name = "Klucz kodowania")]
+        [Required(ErrorMessage = "Musisz podać liczbę -20,20")]
+        [Range(-20, 20, ErrorMessage = "Podaj liczbę z przedziału -20,20")]
+        public int? Key { get; set; }
 
         public bool EncText() {
             string result = "";
---
+++ MultiProjectsWebRazor/MultiProjectsWebRazor.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MultiProjectsWebRazor.Domain;
+
+namespace MultiProjectsWebRazor.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        
+        [BindProperty]
+        public Encrypt Encrypt { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost() {
+            if (ModelState.IsValid) {
+                ViewData["IsPost"] = Encrypt.EncText();
+            }
+        }
+    }
+}
