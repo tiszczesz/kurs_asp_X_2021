de2bb4324beda37983094794166bcc83685b90b3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-11-06 10:55:53 +0100

    rr

---
+++ MultiProjectsWebRazor/MultiProjectsWebRazor.Domain/Encrypt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiProjectsWebRazor.Domain
+{
+    public class Encrypt
+    {
+        public string PlainText { get; set; }
+        public string EncodingText { get; set; }
+        public string DecodingText { get; set; }
+        public int Key { get; set; }
+
+        public bool EncText() {
+            string result = "";
+            foreach (char c in PlainText) {
+                result += EncodeChar(c);
+            }
+
+            EncodingText = result;
+            return true;
+        }
+
+        public bool DecText() {
+            return false;
+        }
+
+        private char EncodeChar(char toEncode) {
+            if (toEncode == ' ') {
+                return ' ';
+            }
+            return (char)((int)toEncode+Key);
+        }
+    }
+}
---
+++ MultiProjectsWebRazor/MultiProjectsWebRazor.UTests/EncodeTest.cs
@@ -0,0 +1,72 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiProjectsWebRazor.Domain;
+
+namespace MultiProjectsWebRazor.UTests
+{
+    [TestClass]
+    public class EncodeTest 
+    {
+        [TestMethod]
+        public void EncodeWithKeyZero()
+        {
+            // Arrange
+            Encrypt encrypt = new Encrypt();
+            encrypt.Key = 0;
+            encrypt.PlainText = "ala ma kota";
+
+            // Act
+            var result = encrypt.EncText();
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(encrypt.PlainText,encrypt.EncodingText);
+        }
+
+        [TestMethod]
+        public void EncodeWithKeyOne()
+        {
+            // Arrange
+            Encrypt encrypt = new Encrypt();
+            encrypt.Key = 0;
+            encrypt.PlainText = "ala ma kota";
+
+            // Act
+            var result = encrypt.EncText();
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual("bmb nb lpub", encrypt.EncodingText);
+        }
+        [TestMethod]
+        public void EncodeOneChar()
+        {
+            // Arrange
+            Encrypt encrypt = new Encrypt();
+            encrypt.Key = 1;
+            encrypt.PlainText = "a";
+
+            // Act
+            var result = encrypt.EncText();
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual("b", encrypt.EncodingText);
+        }
+
+        [TestMethod]
+        public void EncodeSpaceChar()
+        {
+            // Arrange
+            Encrypt encrypt = new Encrypt();
+            encrypt.Key = 1;
+            encrypt.PlainText = " ";
+
+            // Act
+            var result = encrypt.EncText();
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(" ", encrypt.EncodingText);
+        }
+    }
+}
