40aad717432cf7dd7500af5a58690464cc402e94
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-12-04 13:50:50 +0100

    kk

---
+++ FirstMVC/FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello() {
+            return "Hello from Asp.core";
+        }
+
+        public IActionResult Hello2() {
+            //return RedirectToAction("Privacy");
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FirstMVC/FirstMVC/Controllers/My1Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVC.Controllers
+{
+    public class My1Controller : Controller
+    {
+        public IActionResult Index(int? id) {
+            ViewData["akcja"] = ControllerContext.ActionDescriptor.ActionName;
+            ViewData["kontroler"] = ControllerContext.ActionDescriptor.ControllerName;
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
