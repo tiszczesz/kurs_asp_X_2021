8c6e7c909ef92e3942cf912117c45d0eca9b33fd
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-12-04 09:54:58 +0100

    ff

--- Interfejsy_Cw/Interfejsy_Cw/Implements/RamSave.cs
+++ Interfejsy_Cw/Interfejsy_Cw/Implements/RamSave.cs
@@ -11,12 +11,16 @@ namespace Interfejsy_Cw.Implements
     {
         public IList<DataAbstract> Load()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Załadowanie z RAM-u..");
+            return new List<DataAbstract> {
+                new MyData("dane1"), new MyData(), new MyData("dane2"),new MyData("ggggg")
+                ,new MyData("hhhh")
+            };
         }
 
         public void Save(IList<DataAbstract> data)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Zapisane do RAM-u..");
         }
     }
 }
--- Interfejsy_Cw/Interfejsy_Cw/Program.cs
+++ Interfejsy_Cw/Interfejsy_Cw/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using Interfejsy_Cw.Abstraction;
+using Interfejsy_Cw.Implements;
 
 namespace Interfejsy_Cw
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            ISaveable myFileSave = new DbSave();
+            IList<DataAbstract> dane = myFileSave.Load();
+            foreach (var d in dane) {
+                Console.WriteLine(d.GetName());
+                Console.WriteLine((d as MyData)?.GetLength());
+                Console.WriteLine((d as MyData)?.ChangeName(false));
+                Console.WriteLine((d as MyData)?.ChangeName());
+            }
         }
     }
 }
