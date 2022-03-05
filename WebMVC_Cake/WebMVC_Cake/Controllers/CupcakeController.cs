6c1b792c945562effb5d6a776b6c68e8301eaafe
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-03-05 13:26:36 +0100

    ff

--- WebMVC_Cake/WebMVC_Cake/Controllers/CupcakeController.cs
+++ WebMVC_Cake/WebMVC_Cake/Controllers/CupcakeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC_Cake.Models;
 using WebMVC_Cake.Repositories;
 
 namespace WebMVC_Cake.Controllers
@@ -11,9 +12,37 @@ namespace WebMVC_Cake.Controllers
             _repository = repository;
             _environment = environment;
         }
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index() {
+            var cupcakes = _repository.GetCupcakes();
+            return View(cupcakes);
+        }
+
+        public IActionResult GetImage(int id) {
+            Cupcake reqCupcake = _repository.GetCupcake(id);
+            if (reqCupcake != null) {
+                string webRootPath = _environment.WebRootPath;
+                string folderPath = "\\images\\";
+                string fullPath = webRootPath + folderPath + reqCupcake.ImageName;
+                if (System.IO.File.Exists(fullPath)) {
+                    FileStream fileStream = new FileStream(fullPath, FileMode.Open);
+                    byte[] fileBytes;
+                    using (BinaryReader br = new BinaryReader(fileStream)) {
+                        fileBytes = br.ReadBytes((int)fileStream.Length);
+                    }
+
+                    return File(fileBytes, reqCupcake.ImageMimeType);
+                }
+                else {
+                    if (reqCupcake.PhotoFile.Length > 0) {
+                        return File(reqCupcake.PhotoFile, reqCupcake.ImageMimeType);
+                    }
+
+                    return NotFound();
+                }
+            }
+            else {
+                return NotFound();
+            }
         }
     }
 }
