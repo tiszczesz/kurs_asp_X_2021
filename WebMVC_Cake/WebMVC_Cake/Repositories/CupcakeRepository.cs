8b92065af938e834f4bd059b8f431f7728b6d351
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2022-03-05 10:21:43 +0100

    gg

---
+++ WebMVC_Cake/WebMVC_Cake/Repositories/CupcakeRepository.cs
@@ -0,0 +1,40 @@
+﻿using WebMVC_Cake.Data;
+using WebMVC_Cake.Models;
+
+namespace WebMVC_Cake.Repositories
+{
+    public class CupcakeRepository :ICupcakeRepository {
+        private readonly CupcakeContext _context;
+
+        public CupcakeRepository(CupcakeContext context) {
+            _context = context;
+        }
+        public IEnumerable<Cupcake> GetCupcakes() {
+            throw new NotImplementedException();
+        }
+
+        public Cupcake GetCupcake(int id) {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCupcake(Cupcake cupcake) {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCupcake(Cupcake cupcake) {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCupcake(int id) {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges() {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Bakery> PopulateBakeriesDropDownList() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebMVC_Cake/WebMVC_Cake/Repositories/ICupcakeRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebMVC_Cake.Models;
+
+namespace WebMVC_Cake.Repositories
+{
+    public interface ICupcakeRepository {
+        IEnumerable<Cupcake> GetCupcakes();
+        Cupcake GetCupcake(int id);
+        void CreateCupcake(Cupcake cupcake);
+
+        void UpdateCupcake(Cupcake cupcake);
+        void DeleteCupcake(int id);
+        void SaveChanges();
+        IQueryable<Bakery> PopulateBakeriesDropDownList();
+    }
+}
